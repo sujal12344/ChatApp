f70c6d5949908bd57c78c22edef524b8352836cb
Sujal <sujalkesharwani220@gmail.com>
2024-04-29 01:30:31 +0530

    some task done

--- components/Receiver.jsx
+++ components/Receiver.jsx
@@ -1,28 +1,42 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useRef, useState } from "react";
 import Profile from "./Profile";
 import MessageComponent from "./MessageComponent";
 import TextField from "./TextField";
 import gsap from "gsap";
 
 export default function Receiver({ color }) {
+  const divRef = useRef(null)
   const [msg, setMsg] = useState("");
 
+  let width;
+  if (window.innerWidth < 768) {
+    width = (window.innerWidth) - 26 - 48 - 80;
+    console.log(width);
+  } else {
+    width = window.innerWidth / 2 - 220;
+  }
   const getMessage = (msg) => {
     msg = msg.trim();
-    gsap.to(".message", {
-      x: 200,
+
+    gsap.to(".messageReceiver", {
+      // x: width,
+    });
+    gsap.from(".messageReceiver", {
+      opacity: 0.1,
+      duration: 5,
+      x: width,
     });
     setMsg(msg);
   };
 
   return (
     <>
-      <div className="">
         <div className="flex justify-start">
           <div
-            className={`message ${color} relative top-[10px] left-[10px] rounded-md ${
+            className={`messageReceiver ${color} relative top-[10px] left-[10px] rounded-md ${
               msg ? `px-6 py-2` : `p-0`
             }`}
+            ref={divRef}
           >
             {msg}
           </div>
@@ -31,7 +45,6 @@ export default function Receiver({ color }) {
             <TextField className="" getMessage={getMessage} />
           </div>
         </div>
-      </div>
     </>
   );
 }
--- components/Sender.jsx
+++ components/Sender.jsx
@@ -1,37 +1,52 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useRef, useState } from "react";
 import Profile from "./Profile";
 import MessageComponent from "./MessageComponent";
 import TextField from "./TextField";
 import gsap from "gsap";
 
-export default function Sender({color}) {
+export default function Receiver({ color }) {
+  const divRef = useRef(null);
   const [msg, setMsg] = useState("");
 
+  let width;
+  if (window.innerWidth < 768) {
+    width = window.innerWidth - 26 - 48 - 80;
+    console.log(width);
+  } else {
+    width = window.innerWidth / 2 - 220;
+  }
   const getMessage = (msg) => {
     msg = msg.trim();
-    gsap.to(".message", {
-      x: 200
+
+    gsap.to(".messageSender", {
+      x: width,
+    });
+    gsap.from(".messageSender", {
+      opacity: 0.1,
+      duration: 5,
+      // x: width,
     });
     setMsg(msg);
   };
 
   return (
     <>
-        <div className="">
-          <div className="flex justify-start">
-            <div
-              className={`message ${color} relative top-[10px] left-[10px] rounded-md ${
-                msg ? `px-6 py-2` : `p-0`
-              }`}
-            >
-              {msg}
-            </div>
+      <div className="">
+        <div className="flex justify-start">
+          <div
+            className={`messageSender ${color} relative top-[10px] left-[10px] rounded-md ${
+              msg ? `px-6 py-2` : `p-0`
+            }`}
+            ref={divRef}
+          >
+            {msg}
+          </div>
 
-            <div className="absolute bottom-[50%] left-[25px]">
-              <TextField className="" getMessage={getMessage} />
-            </div>
+          <div className="absolute bottom-[50%] left-[25px]">
+            <TextField className="" getMessage={getMessage} />
           </div>
         </div>
+      </div>
     </>
   );
 }
