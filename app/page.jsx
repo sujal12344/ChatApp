e50b7801a92559a2d5e64fe1d68c1ee2de019ac4
Sujal <sujalkesharwani220@gmail.com>
2024-04-30 12:42:16 +0530

    bg change

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import Chat from "@/components/Chat";
 export default function Home() {
   return (
     <div className="grid md:grid-cols-2 grid-rows-2 md:gap-x-4 gap-y-3 md:py-2 md:px-4 p-3 box-border h-screen">
-      <Chat color={"[#753689]"} position={"bottom-[50%]"} />
+      <Chat color={"[#3b7a1d]"} position={"bottom-[50%]"} />
       <Chat color={"[#ff00f7]"} position={"bottom-[3%] "} />
     </div>
   );
--- components/Chat.jsx
+++ components/Chat.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Sender from "./Sender"
 
 export default function Chat({color, position}) {
-  // let Color= color;
-  // console.log(Color);
+  let borderColor= `border-${color}`;
+  let bgColor= `bg-${color}`;
+  console.log(borderColor);
   return (
     <>
-      <div className={`md:col-span-1 row-span-1 border-4 border-solid border-${color} rounded-2xl bg-[#20272b]`}>
-        <Sender color={`bg-${color}`} position={position}/>
+      <div className={`md:col-span-1 row-span-1 border-4 border-solid ${borderColor} rounded-2xl bg-[#20272b]`}>
+        <Sender color={`${bgColor}`} position={position}/>
       </div>
     </>
   );
