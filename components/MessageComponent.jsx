83b7cff3b6de7b445ba9bd3258acec4a36450a28
Sujal <sujalkesharwani220@gmail.com>
2024-05-02 02:45:10 +0530

    Primary Secondary Added

--- components/MessageComponent.jsx
+++ components/MessageComponent.jsx
@@ -1,20 +1,20 @@
 export default function MessageComponent({
   className,
-  color,
+  primaryColor,
+  secondaryColor,
   msg,
   isMessageReceive,
-  flag,
 }) {
   return (
     <>
       <div
         className={`${isMessageReceive ? `flex justify-start` : `flex justify-end w-[92vw]`} 
-        ${className} first:pt-[10px]`}
+        ${className} first:pt-[5px]`}
       >
         <div
-          style={{ background: color }}
-          className={`${isMessageReceive ? `messageReceive` : `messageSend`} 
-          relative mx-[10px] my-[5px] w-fit rounded-md 
+          style={{ backgroundImage: `linear-gradient(to top left, ${primaryColor}, ${secondaryColor})` }}
+          className={` 
+          relative mx-[10px] my-[5px] w-fit max-w-[220px] rounded-md 
           ${msg ? `px-6 py-2` : `p-0`}`}
         >
           {msg}
