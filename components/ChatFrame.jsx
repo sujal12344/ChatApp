c9424556e0b1d642063aa7198cb1ac87b984f5e1
Sujal <sujalkesharwani220@gmail.com>
2024-05-05 01:47:32 +0530

    Some

--- components/ChatFrame.jsx
+++ components/ChatFrame.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import TextField from "./TextField";
 import gsap from "gsap";
 import MessageComponent from "./MessageComponent";
-import { useGSAP } from "@gsap/react";
 
 export default function ChatFrame({
   setPassValue,
@@ -15,54 +14,49 @@ export default function ChatFrame({
   const [messages, setMessages] = useState([]);
   let msgArray = [];
   const OppositeScreen = (msg) => {
-    console.log("value", value);
-    if (value && code === value) {
-      msgArray = [`Hello`, `Hi`, `hello`, `hi`];
+    msgArray = [`Hello`, `Hi`, `hello`, `hi`];
 
-      if (msg === "Hello" || msg === "hello") {
-        receiveMessage("Hi");
-      } else if (msg === "Hi" || msg === "hi") {
-        receiveMessage("Hello");
-      } else if (msg === "how are you") {
-        receiveMessage("I am fine, and you?");
-      } else if (msg === "i am fine") {
-        receiveMessage("That's great to hear.");
-      } else if (msg === "what is your name") {
-        receiveMessage("I am a chatbot.");
-      } else if (msg === "what is your age") {
-        receiveMessage("I am a chatbot, I don't have age.");
-      } else if (msg === "tell me something about you") {
-        receiveMessage("I am a chatbot");
-      } else if (msg === "") {
-        receiveMessage("Please type something.");
-      } else if (msg !== "") {
-        receiveMessage("I am a chatbot, I am here to help you.");
-      }
+    if (msg === "Hello" || msg === "hello") {
+      receiveMessage("Hi");
+    } else if (msg === "Hi" || msg === "hi") {
+      receiveMessage("Hello");
+    } else if (msg === "how are you") {
+      receiveMessage("I am fine, and you?");
+    } else if (msg === "i am fine") {
+      receiveMessage("That's great to hear.");
+    } else if (msg === "what is your name") {
+      receiveMessage("I am a chatbot.");
+    } else if (msg === "what is your age") {
+      receiveMessage("I am a chatbot, I don't have age.");
+    } else if (msg === "tell me something about you") {
+      receiveMessage("I am a chatbot");
+    } else if (msg === "") {
+      receiveMessage("Please type something.");
+    } else if (msg !== "") {
+      receiveMessage("I am a chatbot, I am here to help you.");
     }
   };
 
   const receiveMessage = (msg) => {
     msg = msg.trim();
 
-    useGSAP(() => {
-      gsap.from(".messageReceive", {
-        opacity: 0,
-        scale: 0,
-        duration: 0.8,
-        delay: 0.1,
-        x: innerWidth / 2,
-        scrub: 5,
-        ease: "bounce",
-        borderRadius: "0",
-      });
+    gsap.from(".messageReceive", {
+      opacity: 0,
+      scale: 0,
+      duration: 0.8,
+      delay: 0.1,
+      x: innerWidth / 2,
+      scrub: 5,
+      ease: "bounce",
+      borderRadius: "0",
+    });
 
-      gsap.from(".able_to_move", {
-        opacity: 0.1,
-        duration: 0.8,
-        x: innerWidth / 2,
-        scrub: 5,
-      });
-    }, {scope: MessageComponent, dependencies: [msg]});
+    gsap.from(".able_to_move", {
+      opacity: 0.1,
+      duration: 0.8,
+      x: innerWidth / 2,
+      scrub: 5,
+    });
 
     setMessages((prevMessages) => [
       ...prevMessages,
