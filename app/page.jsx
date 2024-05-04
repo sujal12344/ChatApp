34fbb082ced14fd1615d00f2339d77748677e6c4
Sujal <sujalkesharwani220@gmail.com>
2024-05-05 02:06:31 +0530

    Some

--- app/page.jsx
+++ app/page.jsx
@@ -19,32 +19,32 @@ let contact1 = 5;
 let contact2 = 6;
 
 export default function Home() {
-  let [isRightId, setIsRightId] = useState(false);
-  if (!isRightId) {
-    chatId = parseInt(prompt("Enter Chat ID:"));
-  }
+  // let [isRightId, setIsRightId] = useState(false);
+  // if (!isRightId) {
+  //   chatId = parseInt(prompt("Enter Chat ID:"));
+  // }
 
-  if (chatId == code) {
-    isRightId = true;
-    alert(`Successfully connected with Chat ID: ${chatId}`);
-    {
-      <div
-        className={`flex justify-center bg-yellow-600 duration-[2] items-center h-screen text-[40px] font-semibold`}
-      >
-        <div>Successfully connected to chat {chatId}</div>
-      </div>;
-    }
-  } else {
-    setIsRightId(false);
-    alert(`Please enter correct Chat ID.`);
-    {
-      <div
-        className={`flex justify-center bg-red-600 duration-[2] items-center h-screen text-[40px] font-semibold`}
-      >
-        <div>Please enter correct Chat ID.</div>
-      </div>;
-    }
-  }
+  // if (chatId == code) {
+  //   isRightId = true;
+  //   alert(`Successfully connected with Chat ID: ${chatId}`);
+  //   {
+  //     <div
+  //       className={`flex justify-center bg-yellow-600 duration-[2] items-center h-screen text-[40px] font-semibold`}
+  //     >
+  //       <div>Successfully connected to chat {chatId}</div>
+  //     </div>;
+  //   }
+  // } else {
+  //   setIsRightId(false);
+  //   alert(`Please enter correct Chat ID.`);
+  //   {
+  //     <div
+  //       className={`flex justify-center bg-red-600 duration-[2] items-center h-screen text-[40px] font-semibold`}
+  //     >
+  //       <div>Please enter correct Chat ID.</div>
+  //     </div>;
+  //   }
+  // }
   return (
     <div className="grid md:grid-cols-2 grid-rows-2 md:gap-x-4 gap-y-3 md:py-2 md:px-4 p-3">
       <DynamicChat
